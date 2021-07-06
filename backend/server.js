684d03f6ef884fb971eaad0e01a6655ea2d9b809
DENNIS-CODES <dennistrevor06@gmail.com>
2021-07-07 02:23:12 +0300

    creating server

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+if (!process.env.MONGODB_URL) {
+    throw new Error('You Must Have A MONGODB_URL In Your .env file ') 
+}
+
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
