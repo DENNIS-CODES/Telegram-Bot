3b4a8149fd2d147ba50cd7727fe1b320cb08b22f
DENNIS-CODES <dennistrevor06@gmail.com>
2021-07-06 15:49:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const { Telegraf } = require('telegraf')
+
+require('dotenv').config()
+
+
+console.log(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
